df4823fc4eb2495c7d8443f54be5e07f5b225a0f
stake2 <kiko12345kiko@hotmail.com>
2020-10-04 22:05:03 -0300

    Modified formfile.php

--- Variables/FormFile.php
+++ Variables/FormFile.php
@@ -4,10 +4,10 @@ $formcmnt = "\n".
 '<div class="'.$computervar.'">
 <form name="'.$formcode.'-comment" method="POST" data-netlify="true">
 <span class="w3-btn '.$spanstyle.'" '.$roundedborderstyle.'><b>'.$formname.':</b><br />
-<textarea type="text" name="'.$formcode.'-name" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea>
+<textarea type="text" name="name" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea>
 </span><br />
 <span class="w3-btn '.$spanstyle.'" '.$roundedborderstyle.'><b>'.$cmntstxts[5].':</b><br />
-<textarea type="text" name="'.$formcode.'-comment" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea>
+<textarea type="text" name="comment" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea>
 <h2><button type="submit" class="w3-btn '.$formbtnstyle.'" style="float:right;margin-top:-10px;'.$roundedborderstyle2.'"><i class="fas fa-paper-plane"></i></button></h2>
 </span>
 </form>
@@ -16,10 +16,10 @@ $formcmnt = "\n".
 <div class="'.$mobilevar.'">
 <form name="'.$formcode.'-comment" method="POST" data-netlify="true">
 <span class="w3-btn '.$spanstyle.'" '.$roundedborderstyle.'><b>'.$formname.':</b><br />
-<textarea type="text" name="'.$formcode.'-name" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea>
+<textarea type="text" name="name" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea>
 </span><br />
 <span class="w3-btn '.$spanstyle.'" '.$roundedborderstyle.'><b>'.$cmntstxts[5].':</b><br />
-<textarea type="text" name="'.$formcode.'-comment" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea> 
+<textarea type="text" name="comment" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea> 
 <h2><button type="submit" class="w3-btn '.$formbtnstyle.'" style="float:right;margin-top:-10px;'.$roundedborderstyle2.'"><i class="fas fa-paper-plane"></i></button></h2>
 </span>
 </form>
@@ -32,10 +32,10 @@ $formwrite = "\n".'
 <div class="'.$computervar.'">
 <form name="'.$formcode.'-write" method="POST" data-netlify="true">
 <span class="w3-btn '.$spanstyle.'" '.$roundedborderstyle.'><b>'.$formname.':</b><br />
-<textarea type="text" name="'.$formcode.'-name" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea>
+<textarea type="text" name="name" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea>
 </span><br />
 <span class="w3-btn '.$spanstyle.'" '.$roundedborderstyle.'><b>'.$writetxts[1].':</b><br />
-<textarea type="text" name="'.$formcode.'-write" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea> 
+<textarea type="text" name="comment" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea> 
 <h2><button type="submit" class="w3-btn '.$formbtnstyle.'" style="float:right;margin-top:-10px;'.$roundedborderstyle2.'"><i class="fas fa-paper-plane"></i></button></h2>
 </span>
 </form>
@@ -47,7 +47,7 @@ $formwrite = "\n".'
 <textarea type="text" name="'.$formcode.'-name" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea>
 </span><br />
 <span class="w3-btn '.$spanstyle.'" '.$roundedborderstyle.'><b>'.$writetxts[1].':</b><br />
-<textarea type="text" name="'.$formcode.'-write" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea> 
+<textarea type="text" name="comment" class="'.$formcolor.' w3-input" '.$roundedborderstyle.'></textarea> 
 <h2><button type="submit" class="w3-btn '.$formbtnstyle.'" style="float:right;margin-top:-10px;'.$roundedborderstyle2.'"><i class="fas fa-paper-plane"></i></button></h2>
 </span>
 </form>
