040a6598e11eb56fef2119f2ae001058c9c5cd26
stake2 <kiko12345kiko@hotmail.com>
2021-02-17 02:07:52 -0300

    Removed remote w3.css from loading into website head in Website CSS and JavaScript Definer.php

--- Variables/Global Files/Website CSS And JavaScript Definer.php
+++ Variables/Global Files/Website CSS And JavaScript Definer.php
@@ -30,7 +30,7 @@ if ($site_is_prototype == false) {
 
 #Website CSS definer
 	$website_css_files = $website_css_file_loader.'
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css" />
+<!-- <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css" /> -->
 <link rel="stylesheet" type="text/css" href="'.$cdn_css.'w3.css" />
 '.$main_css.'
 '.$colors_css.'
