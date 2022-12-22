4d949085a402f5d67707631bc03ed9148181e3e2
Stake2 <kiko12345kiko@hotmail.com>
2022-12-22 20:57:52 -0300

    I fixed the "The Secret of the Crystals" website and changed the style of the "Desert Island" and "New World" websites

--- Remake/Websites/New World/Website.php
+++ Remake/Websites/New World/Website.php
@@ -1,17 +1,5 @@
 <?php 
 
-# Website settings
-$website_settings["tab_body_generator"] = True;
-$website_settings["has_stories_tab"] = True;
-#$website_settings["variable_inserter"] = True;
-$website_settings["use_custom_tab_titles"] = True;
 
-$story_website_settings["chapter_opener"] = True;
-$story_website_settings["has_reads"] = True;
-$story_website_settings["has_titles"] = True;
-$story_website_settings["use_status"] = True;
-$story_website_settings["has_story_covers"] = False;
-
-$website_information["theme_color"] = "#5cc6ca";
 
 ?>
